ad99d901eb7fae718511246de7cc48713c659684
Mitansh Patel <mitanshpatel8@gmail.com>
2023-05-02 13:44:54 +0530

    bcrypt

--- app.js
+++ app.js
@@ -3,14 +3,18 @@
 //git status
 //git add .
 //git commit -m ""
+//git remote add origin https://github.com/MitanshPatel/secretsAuth.git
 //git push -u origin master
 
-require('dotenv').config()
+require('dotenv').config()     //to create .env file and save api keys and not get declared in github
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+//const md5 = require("md5");        //bcrypt is better for security
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 //console.log(process.env.SECRET);
 
@@ -25,8 +29,8 @@ const userSchema = new mongoose.Schema({        //obj created frm mongoose schem
     password: String
 });
 
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });     //documentation of mongoose-encrypt
+//this is low level encryption, use md5(hash) instead
+//userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });     //documentation of mongoose-encrypt
 
 const User = new mongoose.model("User", userSchema);
 
@@ -43,14 +47,21 @@ app.get("/register", function(req,res){
 });
 
 app.post("/register", function(req,res){
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    });
-    newUser.save()                //encryption will encrypt when save and decrypt when find
-    .then(function(){
-        console.log("successfully registered");
-        res.render("secrets");
+
+    bcrypt.hash(req.body.password, saltRounds) 
+    .then(function(hash){                 //if above bcrypt fetches the data, then only run this
+        const newUser = new User({
+            email: req.body.username,
+            password: hash                //md5(req.body.password) if md5 module used
+        });
+        newUser.save()                //encryption will encrypt when save and decrypt when find
+        .then(function(){
+            console.log("successfully registered");
+            res.render("secrets");
+        })
+        .catch(function(err){
+            console.log(err);
+        })
     })
     .catch(function(err){
         console.log(err);
@@ -59,15 +70,16 @@ app.post("/register", function(req,res){
 
 app.post("/login", function(req,res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = req.body.password;        //md5(req.body.password) if md5 used
 
     User.findOne({email: username})
     .then(function(foundUser){
-        if(foundUser){
-            if(foundUser.password === password){
+        bcrypt.compare(req.body.password, foundUser.password)
+        .then(function(result){
+            if(result == true){
                 res.render("secrets");
             }
-        }
+        })
     })
     .catch(function(err){
         console.log(err);
